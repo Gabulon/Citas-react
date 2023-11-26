7d81c38d8b8a383ac80300af58c7546e6544fa9d
Gabulon <gabo.fajardo7@gmail.com>
2023-11-26 15:50:41 -0600

    Creando y llamando primer componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
+import Header from "./components/Header";
 function App() {
-  
-  // const sumar = () => {
-  //   const numero=5;
-  //   if(numero>5){
-  //     console.log("si es mayor");
-  //   }else{
-  //     console.log('No es mayor');
-  //   }
-  // }
-  const edad =18;
 
   return (
     <>
-    {edad>= 18 ? 'Eres mayor de edad' : 'No eres mayor de edad'}
-    <div className='App'>
-      <h1>{'Hola Mundo'.toUpperCase()}</h1>
-    </div>
+      <Header/>
     </>
   )
 }
