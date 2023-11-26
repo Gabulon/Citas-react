27346df806537741b29d415623ab76d16928b25c
Gabulon <gabo.fajardo7@gmail.com>
2023-11-26 14:32:26 -0600

    JavaScript en HTML con JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 function App() {
   
+  // const sumar = () => {
+  //   const numero=5;
+  //   if(numero>5){
+  //     console.log("si es mayor");
+  //   }else{
+  //     console.log('No es mayor');
+  //   }
+  // }
+  const edad =18;
+
   return (
     <>
+    {edad>= 18 ? 'Eres mayor de edad' : 'No eres mayor de edad'}
     <div className='App'>
-      <h1>Hola Mundo</h1>
-    </div>
-    <div className='App'>
-      <h1>Hola Mundo</h1>
+      <h1>{'Hola Mundo'.toUpperCase()}</h1>
     </div>
     </>
   )
