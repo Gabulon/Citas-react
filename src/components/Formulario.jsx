19bc552f74b5437c3bcaa156e57f534d793d7996
Gabulon <gabo.fajardo7@gmail.com>
2023-12-03 16:42:35 -0600

    Creando el componente formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,14 @@
 function Formulario(){
 
     return(
-        <h1>Formulario</h1>
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+            <p className="text-lg mt-5 text-center">
+                Añade Pacientes y {' '}
+                <span className="text-indigo-600 font-bold ">Administralos</span>
+            </p>
+        </div>
+        
     )
 }
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,9 @@
 function ListadoPacientes(){
 
     return(
+        <div className="md:w-1/2 lg:w-3/5">
         <h2>Listado</h2>
+        </div>
     )
 }
 
