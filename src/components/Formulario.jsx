83cdb0704ad6d8a473460620949abbb1ab670704
Gabulon <gabo.fajardo7@gmail.com>
2023-12-04 19:05:22 -0600

    leyendo información del resto de los input con useState

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,17 +2,25 @@
 
 function Formulario(){
 
-    const [Nombre,setNombre]=useState('');
+    const [nombre,setNombre]=useState('');
+    const [propietario,setPropietario]=useState('');
+    const [email,setEmail]=useState('');
+    const [fecha,setFecha]=useState('');
+    const [sintomas,setSintomas]=useState('');
     
+    const handleSubmit =(e) =>{
+        e.preventdefault();
+    }
 
     return(
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5 ">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
             <p className="text-lg mt-5 text-center mb-10">
                 Añade Pacientes y {' '}
                 <span className="text-indigo-600 font-bold ">Administralos</span>
             </p>
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font bold">
                         Nombre Mascota</label>
@@ -33,7 +41,8 @@ function Formulario(){
                     id="propietario"
                     placeholder="Nombre del propietario"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-
+                    value={propietario}
+                    onChange={(e)=>setPropietario(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -44,7 +53,8 @@ function Formulario(){
                     id="email"
                     placeholder="Email contacto propietario"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -54,7 +64,8 @@ function Formulario(){
                     <input type="date" 
                     id="alta"
                     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-
+                    value={fecha}
+                    onChange={(e)=>setFecha(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -65,6 +76,8 @@ function Formulario(){
                     id="sintomas"
                     className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                     placeholder="Describe los sintomas"
+                    value={sintomas}
+                    onChange={(e)=>setSintomas(e.target.value)}
                     />
                 </div>
                 <input
