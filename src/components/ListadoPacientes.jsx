a02fc68679ded2a49f4291da5736dcbe31a87148
Gabulon <gabo.fajardo7@gmail.com>
2023-12-24 17:07:58 -0600

    Mostrar Textos de forma Condicional en React

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,8 +2,12 @@ import Paciente from "./Paciente";
 
 function ListadoPacientes({pacientes}){
 
+
+
     return(
         <div className="md:w-1/2 lg:w-3/5  md:h-screen overflow-scroll">
+            {pacientes && pacientes.length ? (
+                <>
                 <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
                 <p className="text-xl mt-5 mb-10 text-center">
                     Administra tus {' '}
@@ -16,8 +20,21 @@ function ListadoPacientes({pacientes}){
                        paciente={paciente}
                        />
                     ))}
-               
-        </div>
+                  
+                  </>
+            ): (
+                <>
+                <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                <p className="text-xl mt-5 mb-10 text-center">
+                    Comienza agregando pacientes {' '}
+                    <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+                </p>
+                </>
+            )}
+           
+                
+            </div>  
+      
     )
 }
 
